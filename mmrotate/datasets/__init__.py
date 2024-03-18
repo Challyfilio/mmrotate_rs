9a6b9e4d19b1938c9220ac80c199b57653c18bb2
✨Challyfilio✨ <challyfilio4368@vip.qq.com>
2024-03-18 16:54:19 +0800

    update mmrotate/datasets

--- mmrotate/datasets/__init__.py
+++ mmrotate/datasets/__init__.py
@@ -4,5 +4,7 @@ from .dota import DOTADataset  # noqa: F401, F403
 from .hrsc import HRSCDataset  # noqa: F401, F403
 from .pipelines import *  # noqa: F401, F403
 from .sar import SARDataset  # noqa: F401, F403
+from .fair import FairDataset
+from .df2023 import DF2023Dataset
 
-__all__ = ['SARDataset', 'DOTADataset', 'build_dataset', 'HRSCDataset']
+__all__ = ['SARDataset', 'DOTADataset', 'build_dataset', 'HRSCDataset', 'FairDataset', 'DF2023Dataset']
