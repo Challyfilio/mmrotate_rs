ef219452a897e377db55bd3161fb1bf46904ff83
✨Challyfilio✨ <challyfilio4368@vip.qq.com>
2024-03-28 08:48:10 +0800

    update config/_base_

--- configs/_base_/datasets/df2023.py
+++ configs/_base_/datasets/df2023.py
@@ -1,12 +1,13 @@
+# Copyright (c) 2023 ✨Challyfilio✨
 # dataset settings
-dataset_type = 'HRSCDataset'
-data_root = 'data/HRSC2016/HRSC2016/'
+dataset_type = 'DF2023Dataset'
+data_root = 'data/split_ss_2023df/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='RResize', img_scale=(800, 800)),
+    dict(type='RResize', img_scale=(1024, 1024)),
     dict(type='RRandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
@@ -17,7 +18,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(800, 800),
+        img_scale=(1024, 1024),
         flip=False,
         transforms=[
             dict(type='RResize'),
@@ -32,25 +33,16 @@ data = dict(
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
-        classwise=False,
-        ann_file=data_root + 'ImageSets/trainval.txt',
-        ann_subdir=data_root + 'FullDataSet/Annotations/',
-        img_subdir=data_root + 'FullDataSet/AllImages/',
-        img_prefix=data_root + 'FullDataSet/AllImages/',
+        ann_file=data_root + 'train/annfiles/',
+        img_prefix=data_root + 'train/images/',
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
-        classwise=False,
-        ann_file=data_root + 'ImageSets/trainval.txt',
-        ann_subdir=data_root + 'FullDataSet/Annotations/',
-        img_subdir=data_root + 'FullDataSet/AllImages/',
-        img_prefix=data_root + 'FullDataSet/AllImages/',
+        ann_file=data_root + 'train/annfiles/',
+        img_prefix=data_root + 'train/images/',
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
-        classwise=False,
-        ann_file=data_root + 'ImageSets/test.txt',
-        ann_subdir=data_root + 'FullDataSet/Annotations/',
-        img_subdir=data_root + 'FullDataSet/AllImages/',
-        img_prefix=data_root + 'FullDataSet/AllImages/',
+        ann_file=data_root + 'train/images/',
+        img_prefix=data_root + 'train/images/',
         pipeline=test_pipeline))
--- configs/_base_/datasets/sodaa.py
+++ configs/_base_/datasets/sodaa.py
@@ -1,12 +1,12 @@
 # dataset settings
-dataset_type = 'DOTADataset'
-data_root = 'data/split_ss_dota/'  ###
+dataset_type = 'SODAADataset'
+data_root = 'data/SODA/SODA-A/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='RResize', img_scale=(1024, 1024)),
+    dict(type='RResize', img_scale=(1200, 1200)),
     dict(type='RRandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
@@ -17,7 +17,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(1024, 1024),
+        img_scale=(1200, 1200),
         flip=False,
         transforms=[
             dict(type='RResize'),
@@ -27,21 +27,31 @@ test_pipeline = [
             dict(type='Collect', keys=['img'])
         ])
 ]
+
 data = dict(
-    samples_per_gpu=2,
+    samples_per_gpu=1,
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
-        ann_file=data_root + 'train/annfiles/',
-        img_prefix=data_root + 'train/images/',
-        pipeline=train_pipeline),
+        angle_version = 'le90',
+        ann_file=data_root + 'divData/train/Annotations/',
+        img_prefix=data_root + 'divData/train/Images/',
+        pipeline=train_pipeline,
+        ori_ann_file=data_root + 'rawData/train/Annotations'
+    ),
     val=dict(
         type=dataset_type,
-        ann_file=data_root + 'val/annfiles/',
-        img_prefix=data_root + 'val/images/',
-        pipeline=test_pipeline),
+        angle_version = 'le90',
+        ann_file=data_root + 'divData/val/Annotations/',
+        img_prefix=data_root + 'divData/val/Images/',
+        pipeline=test_pipeline,
+        ori_ann_file=data_root + 'rawData/val/Annotations'
+    ),
     test=dict(
         type=dataset_type,
-        ann_file=data_root + 'test/images/',
-        img_prefix=data_root + 'test/images/',
-        pipeline=test_pipeline))
+        angle_version = 'le90',
+        ann_file=data_root + 'divData/test/Annotations/',
+        img_prefix=data_root + 'divData/test/Images/',
+        pipeline=test_pipeline,
+        ori_ann_file=data_root + 'rawData/test/Annotations'
+    ))
