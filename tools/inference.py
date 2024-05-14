8b9d48b253c77a12cfa907dec96c8bc2b596c116
✨Challyfilio✨ <challyfilio4368@vip.qq.com>
2024-05-14 08:46:29 +0800

    Update inference.py

--- tools/inference.py
+++ tools/inference.py
@@ -43,25 +43,54 @@ def get_file_basename(path: str) -> str:
 
 
 def vis_rotated_result(image_path: str, config: dict) -> str:
-    outfile_path = ('/workspace/pycharm_project/mmrotate/demo/result_' + get_file_basename(image_path) +
-                    config['outfile'] + '.jpg')
+    # outfile_path = ('/workspace/pycharm_project/mmrotate/demo/result_' + get_file_basename(image_path) +
+    #                 config['outfile'] + '.jpg')
+
+    # 适应项目后台需要
+    root_path = '/workspace/pycharm_project/code/app'
+    project_path = '/static/output_images/'
+    file_name = 'result_' + get_file_basename(image_path) + config['outfile'] + '.jpg'
+
+    outfile_path = root_path + project_path + file_name
+    return_path = project_path + file_name
+
     config_path = config['config_path']
     checkpoint_path = config['checkpoint_path']
+
     # 拼接命令字符串
-    cmd = 'python ../demo/image_demo.py' + ' ' + image_path + ' ' + config_path + ' ' + checkpoint_path + ' --out-file ' + outfile_path
+    cmd = 'python /workspace/pycharm_project/mmrotate/demo/image_demo.py' + ' ' + image_path + ' ' + config_path + ' ' + checkpoint_path + ' --out-file ' + outfile_path
+    logger.info(cmd)
+
     # 执行命令字符串
     os.system(cmd)
     if os.path.exists(outfile_path):
         logger.success('success')
-        return outfile_path
+        return return_path
     else:
         logger.error('failure')
         return ''
 
 
+# 后端 django 接口
+def mmrotate_api(image_path, method):
+    if method == 1:
+        logger.debug(method)
+        outfile_path = vis_rotated_result(image_path, cfg_exp1)
+    elif method == 2:
+        logger.debug(method)
+        outfile_path = vis_rotated_result(image_path, cfg_exp2)
+    elif method == 3:
+        logger.debug(method)
+        outfile_path = vis_rotated_result(image_path, cfg_exp3)
+    else:
+        pass
+
+    return outfile_path
+
+
 if __name__ == '__main__':
-    image_path = '/workspace/pycharm_project/mmrotate/demo/21406.jpg'
+    image_path = '/workspace/pycharm_project/mmrotate/demo/24017.jpg'
     # vis_rotated_result(image_path, cfg_baseline)
     # vis_rotated_result(image_path, cfg_exp1)
-    # vis_rotated_result(image_path, cfg_exp2)
-    vis_rotated_result(image_path, cfg_exp3)
+    vis_rotated_result(image_path, cfg_exp2)
+    # vis_rotated_result(image_path, cfg_exp3)
